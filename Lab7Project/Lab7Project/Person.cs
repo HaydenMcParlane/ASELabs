e81d77904573510bdd49b3e3b9efd3a84b217d0e
Hayden McParlane <hayden.mcparlane@gmail.com>
2015-03-04 23:22:07 -0600

    Current Lab 7 Projects.
    This commit includes the lab 7 project following the tutorial outlined
    in the powerpoint as well as the lab 7 task project which contains the
    code used to satisfy the criteria for the lab 7 task.

---
+++ Lab7Project/Lab7Project/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab7Project
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Company { get; set; }
+        public string Position { get; set; }
+    }
+}
