6dcbda3807b87ed713269ebb3a2962800c4ac0f8
Hayden McParlane <hayden.mcparlane@gmail.com>
2015-03-06 20:42:36 -0600

    Created Service, JavaScript/HTML Frame, JavaScript REST Access Unsuccessful
    - Lab7Service is lab 7 REST service implementation. Fully operational.
    - Login.js doesn't successfully communicate with service. Error 405:
    Method Not Allowed.

---
+++ Lab7Service/Lab7Service/Login.js
@@ -0,0 +1,38 @@
+﻿const LOGIN_BASE = "localhost:59510/Login.svc/";
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+function auth() {
+    //Grab username and password from user entry
+    user = $('username').textContent;
+    pass = $('password').textContent;
+
+    //Use REST Service
+    var url = LOGIN_BASE + user.toString() + "/" + pass.toString();
+
+    var isRegistered = httpGet(url);
+
+    if (isRegistered === 'true') {
+        //Return welcome msg
+        window.location.href = "Welcome.html";
+        $('usr-wel').innerHTML = "Welcome " + user;
+
+    } else if(isRegistered === 'false'){
+        //User needs to register
+        $('login-err').innerHTML = "You haven't registered yet. Please register before logging in.";
+    } else {
+        $('login-err').innerHTML = "A URL Error Occurred.";
+    }    
+}
+
+function httpGet(url) {
+    var xmlHttp = null;
+
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", url, true);    
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+
+}
---
+++ Lab7Service/Lab7Service/Login.svc.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Lab7Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Login" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Login.svc or Login.svc.cs at the Solution Explorer and start debugging.
+    public class Login : ILogin
+    {       
+        [WebInvoke(Method="POST", ResponseFormat=WebMessageFormat.Json, UriTemplate = "/{username}/{password}")]
+        public bool Authenticate(string username, string password)
+        {                        
+            bool resp = false;                               
+
+            //Connect to DB
+            SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\v11.0;AttachDbFilename=C:\\Users\\Hayden\\Documents\\Lab7DB.mdf;Integrated Security=True;Connect Timeout=30");
+
+            //Open connection
+            conn.Open();
+
+            //Crude authentication
+            SqlCommand cmd = new SqlCommand("Select * From Users Where Username = '" + username + "' AND "
+                + "Password = '" + password + "'", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {                    
+                resp = true;
+            }
+
+            cmd.Dispose();
+            conn.Close();
+
+            return resp;
+        }
+
+        [WebInvoke(Method="POST", ResponseFormat=WebMessageFormat.Json, UriTemplate="/Register/{username}/{password}")]
+        public bool Register(string username, string password)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\v11.0;AttachDbFilename=C:\\Users\\Hayden\\Documents\\Lab7DB.mdf;Integrated Security=True;Connect Timeout=30");
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("Insert Into Users(Username, Password) Values ('" + username + "', '" + password + "')", conn);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                conn.Close();
+
+                return true;
+            }
+            catch (Exception b)
+            {
+                Console.WriteLine(b);
+                return false;
+            }
+        }
+    }
+}
